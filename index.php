240f31f55cbbc338fb6b3139c3d6183e413bbde1
Rick Smoke <rsmoke@umich.edu>
2016-01-18 16:27:01 -0500

    Change libraries to CDN based

--- index.php
+++ index.php
@@ -1,24 +1,25 @@
-<?php 
+<?php
 require_once($_SERVER["DOCUMENT_ROOT"] . '/../Support/configSocRefLetV2.php');
 ?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html>
 <head>
-    <title><?php echo "$deptShtName";?> Ref Letter Request - UM Department of <?php echo "$deptLngName";?></title> 
-    
-	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />   
+    <title><?php echo "$deptShtName";?> Ref Letter Request - UM Department of <?php echo "$deptLngName";?></title>
 
-	<link rel="stylesheet" href="css/bootstrap.min.css" />	
-    <link rel="stylesheet" href="css/jquery-ui-1.11.1.min.css" type="text/css" />
+	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+<!--     <link rel="stylesheet" href="css/jquery-ui-1.11.1.min.css" type="text/css" />
     <link rel="stylesheet" href="css/jquery-ui-1.11.1.structure.min.css" type="text/css" />
-    <link rel="stylesheet" href="css/jquery-ui-1.11.1.theme.min.css" type="text/css" />
+    <link rel="stylesheet" href="css/jquery-ui-1.11.1.theme.min.css" type="text/css" /> -->
+    <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
 
-	<script src="js/jquery-1.10.1.min.js"></script>
-	<script src="js/jquery-ui-1.11.1.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
 
 	<link rel="stylesheet" href="css/default.css" type="text/css" />
-    <link rel="stylesheet" href="css/jobRef.css" type="text/css" />		
+    <link rel="stylesheet" href="css/jobRef.css" type="text/css" />
 </head>
 <body>
 <?php echo $login_name ?>
@@ -26,40 +27,40 @@ require_once($_SERVER["DOCUMENT_ROOT"] . '/../Support/configSocRefLetV2.php');
 <div id="Container">
 
 	<div id="MainContent">
-		<div id="Banner"><a href="http://www.lsa.umich.edu/<?php echo strtolower("$deptShtName");?>"><img src="images/banner<?php echo "$deptShtName";?>.png" alt="<?php echo "$deptLngName";?> Home page" /></a></div><!-- #Banner -->	
+		<div id="Banner"><a href="http://www.lsa.umich.edu/<?php echo strtolower("$deptShtName");?>"><img src="images/banner<?php echo "$deptShtName";?>.png" alt="<?php echo "$deptLngName";?> Home page" /></a></div><!-- #Banner -->
 		<div id="leftCol" class="column">
 			<div id="instructions">
 				<h1><?php echo "$deptLngName";?> Reference Letter Requests</h1>
 				<p>The Department of <?php echo "$deptLngName";?> provides the service of sending Student Reference Letters for job applicants. If you would like the Graduate office
-				 to send a letter for you, please use the button below to request this service. You will want to be sure that the faculty writers of your 
+				 to send a letter for you, please use the button below to request this service. You will want to be sure that the faculty writers of your
 				 letters are registered by looking at the listing in the beige box.
 				 You are able to view past submitted requests by clicking the ID number next to the letter description in the table.
-				</p> 
+				</p>
 			</div><!-- #instructions -->
 			<div id="loginHeader">
 				You are logged in as: <span class="loginUser"><?php echo($login_name) ?></span>  <br />(if you are not <?php echo($login_name) ?> please <a href="https://weblogin.umich.edu/cgi-bin/logout">log out</a> and log in again)
-			</div> <!-- loginHeader -->				
+			</div> <!-- loginHeader -->
 			<div>
 				<div id="writerBox">
 					<h5>These are your letter writers.</h5>
 					<div id="writers"></div>
-					<br />				
-				</div><!-- #writerBox -->	
+					<br />
+				</div><!-- #writerBox -->
 				<form id="myForm">
 					If you would like to register another letter writer please enter their <b>uniqname</b> here
 					<input type="text" name="name" />
-					<button id="newWriter" type="button" class="btn btn-info btn-xs">Add</button><br /><i>--look up uniqnames using the <a href="https://mcommunity.umich.edu/" target="_blank">Mcommunity directory</a>.</i>	   	
+					<button id="newWriter" type="button" class="btn btn-info btn-xs">Add</button><br /><i>--look up uniqnames using the <a href="https://mcommunity.umich.edu/" target="_blank">Mcommunity directory</a>.</i>
 				</form><!-- myForm -->
 			</div><!-- form section-->
 			<hr />
 			<div>
 				<p>These are your submitted requests.</p>
 				<button id="reqSub" type="button" class="btn btn-info btn-xs">Add a New Request</button>&nbsp;&nbsp;<button id="indDwnld" class="btn btn-default btn-xs" type="button">Download Archive</button><br />
-				<div id="reqSubmitted">	
+				<div id="reqSubmitted">
 					<?php include("php/userSubmitted.php"); ?>
-				</div> <!-- #reqSubmitted -->		
-			</div><!-- submitted requests section -->			
-		</div><!-- #leftCol -->	
+				</div> <!-- #reqSubmitted -->
+			</div><!-- submitted requests section -->
+		</div><!-- #leftCol -->
 		<div id="rightColUser">
 			<h1 class="rtCol">QUICK LINKS</h1>
 			<ul>
@@ -68,37 +69,37 @@ require_once($_SERVER["DOCUMENT_ROOT"] . '/../Support/configSocRefLetV2.php');
 			</ul>
 			</div> <!-- #rightCol -->
 		</div> <!-- #MainContent -->
-	<div id="footer"> 
-	<p>   
-	    <?php echo "$deptLngName";?>      
+	<div id="footer">
+	<p>
+	    <?php echo "$deptLngName";?>
 		<br />LSA Building
 	    <br />Room 3001
 	    <br />500 S. State Street
 		<br />Ann Arbor, MI &nbsp;48109-1382
-  	</p> 
-  	
+  	</p>
+
 	<p>
 		ph: 734-764-6324
-		<br /> 
+		<br />
  	   fx: 734-763-6887
- 	</p> 
+ 	</p>
 
 	<p id="ftr-contact">
-		e:<a href="mailto:<?php echo strtolower("$deptLngName");?> .department@umich.edu"><?php echo strtolower("$deptLngName");?> .department@umich.edu</a> 
-	
-	    <a href="http://www.facebook.com/universityofmichigan.<?php echo strtolower("$deptLngName");?> ">Find us on Facebook!</a> 
-		&nbsp;|&nbsp;<a href="http://www.lsa.umich.edu/<?php echo strtolower("$deptShtName");?>/sitemap">Sitemap</a> 
-	    </p> 
-    
-	<p id="UMlogos"><a href="http://www.umich.edu"><img src="images/michigan.png" alt="University of Michigan" /></a></p> 
-	
-	<p id="logos"><a href="http://www.lsa.umich.edu"><img src="images/lsa.png" alt= "LSA" /></a></p> 
+		e:<a href="mailto:<?php echo strtolower("$deptLngName");?> .department@umich.edu"><?php echo strtolower("$deptLngName");?> .department@umich.edu</a>
+
+	    <a href="http://www.facebook.com/universityofmichigan.<?php echo strtolower("$deptLngName");?> ">Find us on Facebook!</a>
+		&nbsp;|&nbsp;<a href="http://www.lsa.umich.edu/<?php echo strtolower("$deptShtName");?>/sitemap">Sitemap</a>
+	    </p>
+
+	<p id="UMlogos"><a href="http://www.umich.edu"><img src="images/michigan.png" alt="University of Michigan" /></a></p>
+
+	<p id="logos"><a href="http://www.lsa.umich.edu"><img src="images/lsa.png" alt= "LSA" /></a></p>
 </div> <!-- #footer -->
-	
+
 </div> <!-- #Container -->
 
 	<script src="js/my_script.js" type="text/javascript"></script>
-</body> 
+</body>
 </html>
 
 <?php
--- php/reqSubmit.php
+++ php/reqSubmit.php
@@ -1,4 +1,4 @@
-<?php  
+<?php
 require_once($_SERVER["DOCUMENT_ROOT"] . '/../Support/configSocRefLetV2.php');
 
 ?>
@@ -6,21 +6,26 @@ require_once($_SERVER["DOCUMENT_ROOT"] . '/../Support/configSocRefLetV2.php');
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html>
 <head>
-    <title><?php echo "$deptShtName";?> Ref Letter Request - UM Department of <?php echo "$deptLngName";?></title> 
-    
+    <title><?php echo "$deptShtName";?> Ref Letter Request - UM Department of <?php echo "$deptLngName";?></title>
+
 	<link rel="shortcut icon" href="../images/favicon.ico" type="image/x-icon" />
 
-	<link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css" />
+<!-- 	<link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css" />
 	<link rel="stylesheet" href="../css/jquery-ui-1.11.1.min.css" type="text/css" />
-		
+
 	<script type="text/javascript" src="../js/jquery-1.10.1.min.js"></script>
-	<script type="text/javascript" src="../js/jquery-ui-1.11.1.min.js"></script>
-	
+	<script type="text/javascript" src="../js/jquery-ui-1.11.1.min.js"></script> -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+
 	<script type="text/javascript" src="../js/formValidator.js"></script>
 
-	<link rel="stylesheet" href="../css/default.css" type="text/css" />	
+	<link rel="stylesheet" href="../css/default.css" type="text/css" />
     <link rel="stylesheet" href="../css/jobRef.css" type="text/css" />
-  		
+
 </head>
 <body>
 
@@ -33,12 +38,12 @@ require_once($_SERVER["DOCUMENT_ROOT"] . '/../Support/configSocRefLetV2.php');
 		<div id="leftCol" class="column">
 			<div id="instructions">
 				<h1><?php echo "$deptLngName";?> Reference Letter Requests Submittal</h1>
-				<p>Please fill in the form below with the information required to send your letter. If you have instructions 
-				for the Graduate office please enter these in the "Special Instructions" area at the bottom of the form</p> 
+				<p>Please fill in the form below with the information required to send your letter. If you have instructions
+				for the Graduate office please enter these in the "Special Instructions" area at the bottom of the form</p>
 			</div><!-- #instructions -->
-			
+
 			<div id="loginHeader">
-				<p>You are logged in as: <span class="loginUser"><?php echo($login_name) ?></span>  (if you are not <?php echo($login_name) ?> please <a href="https://weblogin.umich.edu/cgi-bin/logout">log out</a> and log in again)</p>	
+				<p>You are logged in as: <span class="loginUser"><?php echo($login_name) ?></span>  (if you are not <?php echo($login_name) ?> please <a href="https://weblogin.umich.edu/cgi-bin/logout">log out</a> and log in again)</p>
 			</div> <!-- loginHeader -->
 			<div><p>&nbsp;</p></div>
 			<div>
@@ -50,10 +55,10 @@ require_once($_SERVER["DOCUMENT_ROOT"] . '/../Support/configSocRefLetV2.php');
 			    	<legend>Reference Letter Details</legend>
 			    	<label class="label" for="posTitle">Job Title:</label>
 			            <input type="text" name="posTitle" id="posTitle" >
-			            
+
 					<label class="label" for="dueDate">Date Due:</label>
 			        	<input type="text" name="dueDate" id="datepicker" >
-			        	
+
 			        	<table>
 			        	<tr><td width="50%">
 		            	<label for="letterType" class="control-label input-group">Request Type</label>
@@ -62,9 +67,9 @@ require_once($_SERVER["DOCUMENT_ROOT"] . '/../Support/configSocRefLetV2.php');
 		            	<label class="label"><input type="radio" name="letterType" id="electUpld" value="eUpload" >&nbsp;Upload&nbsp;<input type="text" name="recipURL" id="recipURL" placeholder="webLink for Upload"></label><br />
 		                <label class="label"><input type="radio" name="letterType" id="hdCpy" value="HardCopy" >&nbsp;Hardcopy</label>
 		               </div>
-			            
+
 			        	</td><td>
-			<?php			
+			<?php
 			$sqlSelect = "SELECT * FROM SRL_tbl_StudentToWriter WHERE FK_student_uniqname = '$login_name'";
 			//Query the database for results we want
 			$resultSelect = mysqli_query($db, $sqlSelect);
@@ -85,7 +90,7 @@ require_once($_SERVER["DOCUMENT_ROOT"] . '/../Support/configSocRefLetV2.php');
 						<?php endforeach; ?>
 					</select>
 					<br />
-					
+
 					<label class = "label" for="writerID2">2nd Writer:</label>
 					<select name="writerID2">
 						<option value="NotSelected"> -- </option>
@@ -94,7 +99,7 @@ require_once($_SERVER["DOCUMENT_ROOT"] . '/../Support/configSocRefLetV2.php');
 						<?php endforeach; ?>
 					</select>
 					<br />
-					
+
 					<label class = "label" for="writerID3">3rd Writer:</label>
 					<select name="writerID3">
 						<option value="NotSelected"> -- </option>
@@ -103,7 +108,7 @@ require_once($_SERVER["DOCUMENT_ROOT"] . '/../Support/configSocRefLetV2.php');
 						<?php endforeach; ?>
 					</select>
 					<br />
-					
+
 					<label class = "label" for="writerID4">4th Writer:</label>
 					<select name="writerID4">
 						<option value="NotSelected"> -- </option>
@@ -112,7 +117,7 @@ require_once($_SERVER["DOCUMENT_ROOT"] . '/../Support/configSocRefLetV2.php');
 						<?php endforeach; ?>
 					</select>
 					<br />
-					
+
 					<label class = "label" for="writerID5">5th Writer:</label>
 					<select name="writerID5">
 						<option value="NotSelected"> -- </option>
@@ -120,11 +125,11 @@ require_once($_SERVER["DOCUMENT_ROOT"] . '/../Support/configSocRefLetV2.php');
 						<option value="<?php echo $option->FK_writer_uniqname; ?>"><?php echo $option->FK_writer_uniqname; ?></option>
 						<?php endforeach; ?>
 					</select>
-			           </td></tr></table> 
+			           </td></tr></table>
 			    </fieldset>
-			
+
 			    <fieldset>
-			    	<legend>Send Letter to:</legend>  
+			    	<legend>Send Letter to:</legend>
 					<label class="label" for="recipTitle">Title:</label>
 			            	<select size="1" name="recipTitle" id="recipTitle">
 			                    <option value="Professor">Professor</option>
@@ -135,74 +140,74 @@ require_once($_SERVER["DOCUMENT_ROOT"] . '/../Support/configSocRefLetV2.php');
 			                    <option value="Ms">Ms</option>
 			                    <option value="other">Other</option>
 			            	</select>
-			            	
+
 			        	<br />
-			        				            	
+
 			        <label class = "label" for="recipFname">First name:</label>
 			            <input type="text" name="recipFname" id="recipFname" >
 
 			        <label class="label" for="recipMname">MI:</label>
 			            <input type="text" name="recipMname" id="recipMname" >
-			            
+
   			        	<br />
-			            
+
 			        <label class = "label" for="recipLname">Last Name:</label>
 			            <input type="text" name="recipLname" id="recipLname" >
-			            
+
 			        <label  class="label" for="recipSuffix">Suffix:</label>
 			            <input type="text" name="recipSuffix" id="recipSuffix" >
-			            
+
     			        	<br />
-			            
+
 			        <label class = "label" for="recipInst">Institution:</label>
 			            <input type="text" name="recipInst" id="recipInst" >
-			            			            
+
     			        	<br />
-			        
+
 			        <label class = "label" for="recipDept">Department:</label>
 			            <input type="text" name="recipDept" id="recipDept" >
-			            			            
+
     			        	<br />
-			        		        
+
 			        <label class = "label" for="recipStreet">Street:</label>
 			            <input type="text" name="recipStreet" id="recipStreet" >
-			            			        
+
 			        <label class="label" for="recipRoom">Room/Suite:</label>
 			            <input type="text" name="recipRoom" id="recipRoom" >
-			        
+
 			        <br />
-			        
+
 			        <label class = "label" for="recipCity">City:</label>
 			            <input type="text" name="recipCity" id="recipCity" >
-			   
+
 			   <br />
-			            			        
+
 			        <label class = "label" for="recipState">State:</label>
 			            <input type="text" name="recipState" id="recipState" >
-			            			        
+
 			        <label class="label" for="recipZip">Zipcode:</label>
 			            <input type="text" name="recipZip" id="recipZip" >
-			            
+
 			            <br />
-			            			        
+
 			        <label class = "label" for="recipCountry">Country:</label>
 			            <input type="text" name="recipCountry" id="recipCountry" >
-			            			        
+
 						<br />
-			            			        
+
 			        <label class = "label" for="recipPhone">Phone:</label>
 			            <input type="text" name="recipPhone" id="recipPhone" >
-			            	
+
 			            	<br />
-			            			        
+
 			        <label class="label" for="recipSpInstx">Special Instructions:</label><br />
-			            <textarea cols ="75" name="recipSpInstx" id="recipSpInstx" rows="5" placeholder="Enter any other information that should be considered when processing this request"></textarea>
+			            <textarea cols ="65" name="recipSpInstx" id="recipSpInstx" rows="5" placeholder="Enter any other information that should be considered when processing this request"></textarea>
 			            <br />
 			    	<button id="newRecordSub" type="submit" class="btn btn-primary">Submit</button>
 			    </fieldset>
 			</form>
 			</div><!-- form -->
-			
+
 		</div><!-- #leftCol -->
 
 		<div id="rightColUser">
@@ -215,31 +220,31 @@ require_once($_SERVER["DOCUMENT_ROOT"] . '/../Support/configSocRefLetV2.php');
 
 	</div> <!-- #MainContent -->
 
-	<div id="footer"> 
-		<p>   
-		    <?php echo "$deptLngName";?>      
+	<div id="footer">
+		<p>
+		    <?php echo "$deptLngName";?>
 			<br />LSA Building
 		    <br />Room 3001
 		    <br />500 S. State Street
 			<br />Ann Arbor, MI &nbsp;48109-1382
-	  	</p> 
-	  	
+	  	</p>
+
 		<p>
 			ph: 734-764-6324
-			<br /> 
+			<br />
 	 	   fx: 734-763-6887
-	 	</p> 
-	
+	 	</p>
+
 		<p id="ftr-contact">
-			e:<a href="mailto:<?php echo strtolower("$deptLngName");?> .department@umich.edu"><?php echo strtolower("$deptLngName");?> .department@umich.edu</a> 
-		
-		    <a href="http://www.facebook.com/universityofmichigan.<?php echo strtolower("$deptLngName");?> ">Find us on Facebook!</a> 
-			&nbsp;|&nbsp;<a href="http://www.lsa.umich.edu/<?php echo strtolower("$deptShtName");?>/aboutus/sitemap">Sitemap</a> 
-		    </p> 
-	    
-		<p id="UMlogos"><a href="http://www.umich.edu"><img src="../images/michigan.png" alt="University of Michigan" /></a></p> 
-		
-		<p id="logos"><a href="http://www.lsa.umich.edu"><img src="../images/lsa.png" alt= "LSA" /></a></p> 
+			e:<a href="mailto:<?php echo strtolower("$deptLngName");?> .department@umich.edu"><?php echo strtolower("$deptLngName");?> .department@umich.edu</a>
+
+		    <a href="http://www.facebook.com/universityofmichigan.<?php echo strtolower("$deptLngName");?> ">Find us on Facebook!</a>
+			&nbsp;|&nbsp;<a href="http://www.lsa.umich.edu/<?php echo strtolower("$deptShtName");?>/aboutus/sitemap">Sitemap</a>
+		    </p>
+
+		<p id="UMlogos"><a href="http://www.umich.edu"><img src="../images/michigan.png" alt="University of Michigan" /></a></p>
+
+		<p id="logos"><a href="http://www.lsa.umich.edu"><img src="../images/lsa.png" alt= "LSA" /></a></p>
 	</div> <!-- #footer -->
 </div> <!-- #Container -->
     <script type="text/javascript">
@@ -250,7 +255,7 @@ require_once($_SERVER["DOCUMENT_ROOT"] . '/../Support/configSocRefLetV2.php');
 	});
 	</script>
 <!-- <script src="../js/my_script.js"></script> -->
-</body> 
+</body>
 </html>
 
 <?php
